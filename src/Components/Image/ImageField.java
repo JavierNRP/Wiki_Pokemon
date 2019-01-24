bf826e26c511ac6398a9f7bbfef99db6e9f77435
JavierNRP <46812691+JavierNRP@users.noreply.github.com>
2019-01-24 15:54:20 +0000

    Image

---
+++ src/Components/Image/ImageField.java
@@ -0,0 +1,91 @@
+package Components.Image;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.IntegerProperty;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
+
+public class ImageField extends BorderPane  implements Initializable  {
+
+	ImageModel model = new ImageModel();
+	
+	@FXML ImageView Image;
+
+    public ImageField() {
+    	try { 
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Image.fxml"));
+			loader.setController(this);
+			loader.setRoot(this);
+			loader.load();
+			model.setImgId(1);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+	}
+
+    public ImageField(int imgId) {
+    	try { 
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Image.fxml"));
+			loader.setController(this);
+			loader.setRoot(this);
+			loader.load();
+			model.setImgId(imgId);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+	}
+    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		model.setRoute("file:///C:\\Users\\JavierN\\Pictures\\pokemon\\main-sprites\\platinum\\");
+		model.imgIdProperty().addListener( e-> setframes() );
+		Image.imageProperty().bind(model.frame1Property());
+		model.setMouseOn(false);
+
+		new Thread(animation).start();
+	}
+	
+	private void setframes() {
+		model.setFrame1(model.getRoute()+model.getImgId()+".png");
+		model.setFrame2(model.getRoute()+"frame2\\"+model.getImgId()+".png");
+	}
+	
+	Task<Void> animation = new Task<Void>() {
+		@Override
+		protected Void call() throws Exception {
+			try {
+				while (true) {
+					TimeUnit.MILLISECONDS.sleep(500);
+					Image.imageProperty().unbind();
+					Image.imageProperty().bind(model.frame2Property());
+					TimeUnit.MILLISECONDS.sleep(500);
+					Image.imageProperty().unbind();
+					Image.imageProperty().bind(model.frame1Property());
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Image.imageProperty().unbind();
+			Image.imageProperty().bind(model.frame1Property());
+			return null;
+		}
+	};
+
+	public final IntegerProperty imgIdProperty() { return model.imgIdProperty(); }
+	public final int getImgId() { return model.getImgId(); }
+	public final void setImgId(final int imgId) { model.setImgId(imgId); }
+
+}
--- src/pruebas/MainImgPruebas.java
+++ src/pruebas/MainImgPruebas.java
@@ -1,14 +1,11 @@
 package pruebas;
 
-import java.sql.Blob;
-
+import Components.Image.ImageField;
 import Components.TypeSelector.TypeSelectorField;
 import Components.Weakness.WeaknessField;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
@@ -19,17 +16,16 @@ public class MainImgPruebas extends Application {
     try {
       WeaknessField testW = new WeaknessField();
       TypeSelectorField testS = new TypeSelectorField();
-      
-      Image image;
-//      Blob ablob = new 
-      ImageView imageview = new ImageView();
+      ImageField testI = new ImageField();
+//      testI.setImgId(2);
       
       Button show = new Button();
       show.setText("Show");
       show.setOnAction(e->testW.setTypes(testS.getType_p(), testS.getType_s()));
       
       VBox root = new VBox();
-      root.getChildren().addAll(testW, testS, show);
+      root.setStyle("-fx-background-color: #ECD82D; ");
+      root.getChildren().addAll(testW, testS, show, testI);
       Scene scene = new Scene(root,400,400);
       primaryStage.setScene(scene);
       primaryStage.show();
