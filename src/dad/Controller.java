151634f6c60565c46f5f4c9b71f9355d700a2f37
Juan Gomez <bz.juan15@gmail.com>
2019-02-11 09:55:39 +0000

    Commit view

---
+++ src/dad/Controller.java
@@ -0,0 +1,43 @@
+package dad;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private AnchorPane pokedex;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public Controller() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("View/MainLayout/main.fxml"));
+            loader.setController(this);
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //Funciones FXML
+    @FXML
+    public void onCloseButton(){
+        System.exit(0);
+    }
+
+    //Getters & Setters
+    public AnchorPane getPokedex() {
+        return pokedex;
+    }
+}
---
+++ src/dad/Models/FichaPokemon.java
@@ -0,0 +1,7 @@
+package dad.Models;
+
+public class FichaPokemon {
+    //Ficha que se muestra en cada pantalla de la pokedex
+    private Pokemon pkm;
+    private String descripcion;
+}
