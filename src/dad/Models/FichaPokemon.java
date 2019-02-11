02fad7392765346f68a561f0fbe48053a160b614
JavierNRP <46812691+JavierNRP@users.noreply.github.com>
2019-02-11 09:25:43 +0000

    Revert "borrar target"
    This reverts commit 49b8506760caca4655c5179bd7b35e5649038772, reversing
    changes made to 975fdc0b835c79195de437d00f70283b0f3713be.

--- src/dad/Models/FichaPokemon.java
+++
@@ -1,7 +0,0 @@
-package dad.Models;
-
-public class FichaPokemon {
-    //Ficha que se muestra en cada pantalla de la pokedex
-    private Pokemon pkm;
-    private String descripcion;
-}
--- src/dad/View/View.java
+++
@@ -1,4 +0,0 @@
-package dad.View;
-
-public class View {
-}
